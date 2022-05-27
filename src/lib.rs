3a394b46805dfe0ca90024897dd94995eb35fce4
YoungHa Kim <ytok1108@aol.com>
2022-05-27 20:19:14 +0900

    recursive type  & Box

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 enum Node {
     Empty,
-    NonEmpty(u32, &'static Node),
+    NonEmpty(u32, Box<Node>),
 }
 
 #[cfg(test)]
@@ -9,7 +9,6 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let node = &Node::Empty;
-        let list = Node::NonEmpty(1091, node);
+        let list = Node::NonEmpty(1091, Box::new(Node::Empty));
     }
 }
