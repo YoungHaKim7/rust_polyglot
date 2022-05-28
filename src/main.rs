3b5c0ae7ab2663de2e22c401ba9b3a22b5ee90aa
YoungHa Kim <ytok1108@aol.com>
2022-05-28 21:44:01 +0900

    Deep Dive trait D&D 003__final

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,25 @@ struct Human {
     name: String,
 }
 
+// All of these need to be cast
+struct Cantrip {}
+
+struct Enchantment {}
+
+struct Transmutation {}
+
+struct Necromancy {}
+
+struct Spellbook {
+    pub spells: Vec<Box<dyn Cast>>,
+}
+
+// Vector contains Boxes
+// <T> <- Type of value Box can point to
+// <Cast> <- Box : must point to a value the implement Cast
+// Vec<T> <- Vector that contain object of a trait
+// How can we cast ALL of these
+
 // The constitution bonus for a dwarf is 2
 impl Constitution for Dwarf {
     fn constitution_bonus(&self) -> u8 {
@@ -49,6 +68,42 @@ pub trait Constitution {
 
 pub trait Elvish {}
 
+impl Spellbook {
+    pub fn run(&self) {
+        for spell in self.spells.iter() {
+            spell.cast(); // Cast spell
+        }
+    }
+}
+
+pub trait Cast {
+    fn cast(&self);
+}
+
+impl Cast for Cantrip {
+    fn cast(&self) {
+        // Details of casting a Cantrip Spell
+    }
+}
+
+impl Cast for Transmutation {
+    fn cast(&self) {
+        // Details of casting a Transmutation Spell
+    }
+}
+
+impl Cast for Enchantment {
+    fn cast(&self) {
+        // Details of casting a Enchantment Spell
+    }
+}
+
+impl Cast for Necromancy {
+    fn cast(&self) {
+        // Details of casting a Necromancy Spell
+    }
+}
+
 impl Elvish for Elf {}
 impl Elvish for HalfElf {}
 impl Elvish for HalfOrc {}
@@ -63,6 +118,19 @@ pub fn speak_elvish<T: Elvish>(character: T) -> String {
 pub fn no_speak_elvish<T: Elvish>(character: T) -> String {
     String::from("no")
 }
+
+// you cannot add data to a trait object
+// Trait objects contain both data(Pointer) and behavior(Trait)
+
+// Spell in D&D (Cantrip, Transmutation, Enchantment, Necromancy)
+
+// Trait objects are great for heterogeneous collections
+// It doesn't matter what type something is, as long as it implements
+// a certain trait
+
+// Trait are hard...... at first
+// Trait are awesome!
+// I have learned to harness the power of traits... And so can you!!!
 
 fn main() {
     let my_dwarf = Dwarf {
@@ -84,6 +152,19 @@ fn main() {
     let my_half_elf = HalfElf {
         name: String::from("NellElf"),
     };
+
+    let spell_book = Spellbook {
+        // Different types of spells (each implement Cast)
+        spells: vec![
+            Box::new(Cantrip {}),
+            Box::new(Transmutation {}),
+            Box::new(Enchantment {}),
+            Box::new(Necromancy {}),
+        ],
+    };
+
+    // Casts each spell
+    spell_book.run();
 
     // Return 2
     my_dwarf.constitution_bonus();
