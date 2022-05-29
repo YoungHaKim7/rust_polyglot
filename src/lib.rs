dbeff3766c08eb6d092dfa988c7fdfd99c3de8cd
YoungHa Kim <ytok1108@aol.com>
2022-05-29 21:54:10 +0900

    error code __cannot infer an appropriate lifetime for autoref due to conflicting requirements

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,23 @@ impl<'a, T> Iterator for MyIterator<'a, T> {
     }
 }
 
+struct MyMutableIterator<'a, T> {
+    slice: &'a mut [T],
+}
+
+impl<'a, T> Iterator for MyMutableIterator<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // get first element
+        let element = self.slice.get_mut(0);
+        // set self.slice to the rest of the list
+        self.slice = &mut self.slice[1..];
+        // return first element
+        element
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -26,5 +43,16 @@ mod tests {
         for (index, elem) in wrapper.enumerate() {
             assert_eq!(*elem, collection[index]);
         }
+
+        let collection = vec![1, 2, 3, 4];
+        let wrapper = MyMutableIterator {
+            slice: &mut &collection[..],
+        };
+
+        for (index, elem) in wrapper.enumerate() {
+            *elem = *elem + 1;
+        }
+
+        assert_eq!(collection.get(0), Some(&2));
     }
 }
