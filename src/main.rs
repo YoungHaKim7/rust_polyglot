4916214a7db1e046ea15a1a5959b0200c3bb01de
YoungHa Kim <ytok1108@aol.com>
2022-05-29 19:02:52 +0900

    len() vs iter() vs iter_mut()

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,24 @@
-fn main() {}
+fn main() {
+    println!("-----");
+    println!("-len()----");
+    let stuff = vec![1, 2, 3];
+    for i in 0..stuff.len() {
+        println!("{}", stuff[i]);
+    }
+
+    println!("-----");
+    println!("-iter()----");
+
+    for elem in stuff.iter() {
+        println!("{}", elem);
+    }
+
+    println!("-----");
+    println!("-iter_mut()----");
+
+    let mut stuff_clone = stuff.clone();
+    for elem in stuff_clone.iter_mut() {
+        *elem += 2;
+        println!("{}", elem);
+    }
+}
