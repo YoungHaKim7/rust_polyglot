b3fb2efd31d9803427aa45a21550ba6bb0fb89ab
YoungHa Kim <ytok1108@aol.com>
2022-05-29 19:08:08 +0900

    cpp style// for (int i = 0 ; i < size; i++)

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,12 @@
-// struct MyIterWrapper {
-//     slice: &[T],
-// }
+struct MyIterWrapper {
+    slice: &[T],
+}
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
-        let collection = &mut [1, 2, 3, 4];
-        for elem in collection.iter_mut() {
-            *elem += 2;
-        }
-        assert_eq!(collection, &[3, 4, 5, 6]);
+        let collection = vec![1, 2, 3, 4];
+        for elem in collection.iter_mut() {}
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,4 @@
 fn main() {
-    println!("-----");
-    println!("-len()----");
-    let stuff = vec![1, 2, 3];
-    for i in 0..stuff.len() {
-        println!("{}", stuff[i]);
-    }
-
-    println!("-----");
-    println!("-iter()----");
-
-    for elem in stuff.iter() {
-        println!("{}", elem);
-    }
-
-    println!("-----");
-    println!("-iter_mut()----");
-
-    let mut stuff_clone = stuff.clone();
-    for elem in stuff_clone.iter_mut() {
-        *elem += 2;
-        println!("{}", elem);
-    }
+    let stuff_cpp_style = vec![1, 2, 3];
+    // for (int i = 0 ; i < size; i++)
 }
