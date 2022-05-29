202a80123d1d8ad7c65afa67bfcfba5f31e54da9
YoungHa Kim <ytok1108@aol.com>
2022-05-29 20:43:44 +0900

    Lifetimes__'a___002

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,25 @@ struct MyIterWrapper<'a, T> {
     slice: &'a [T],
 }
 
+impl<'a, T> Iterator for MyIterWrapper<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        todo!();
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
         let mut collection = vec![1, 2, 3, 4];
-        for elem in collection.iter_mut() {}
+        let wrapper = MyIterWrapper {
+            slice: &collection[..],
+        };
+
+        for elem in wrapper {}
     }
 }
