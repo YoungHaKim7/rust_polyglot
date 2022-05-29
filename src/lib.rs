fed5f6ba934b52dd43c2d3df77f5b1444959f67c
YoungHa Kim <ytok1108@aol.com>
2022-05-29 18:41:11 +0900

    Lifetime002__error code

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
+struct MyIterWrapper {
+    slice: &[T],
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let collection = vec![1, 2, 3, 4];
+        for elem in collection.iter_mut() {}
     }
 }
