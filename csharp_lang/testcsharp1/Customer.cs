14fd924af97cf2233ef3b2d58851805e0024b99e
YoungHa Kim <ytok1108@aol.com>
2022-05-30 21:04:12 +0900

    C# good

---
+++ csharp_lang/testcsharp1/Customer.cs
@@ -0,0 +1,8 @@
+namespace helloworldexample
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
--- csharp_lang/testcsharp1/Program.cs
+++ csharp_lang/testcsharp1/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace helloworldexample
+{
+    class Program
+    {
+        static void main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Customer customer = new Customer();
+            customer.FirstName = "customer";
+            customer.LastName = "one";
+        }
+    }
+
+}
