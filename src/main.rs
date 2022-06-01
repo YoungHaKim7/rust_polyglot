7907747a95c34ade7c651c9b4bb62e576c4d7724
YoungHa Kim <ytok1108@aol.com>
2022-06-01 23:33:05 +0900

    tuple training

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,4 @@
-use std::ops::Deref;
-use std::ops::DerefMut;
-
-struct MyBox<T>(T);
-
-impl<T> MyBox<T> {
-    fn new(x: T) -> MyBox<T> {
-        MyBox(x)
-    }
-}
-
-impl<T> Deref for MyBox<T> {
-    type Target = T;
-
-    fn deref(&self) -> &T {
-        &self.0 // tuple  tuple.0
-    }
-    // *y -> *(y.deref())
-}
-
-impl<T> DerefMut for MyBox<T> {
-    fn deref_mut(&mut self) -> &mut T {
-        &mut self.0
-    }
-}
-
-struct Whatever(i32);
-
-impl Drop for Whatever {
-    fn drop(&mut self) {
-        println!("Dropping Whatever with data: {}!", self.0);
-    }
-}
 fn main() {
-    // let x = 5;
-    // let mut y = MyBox::new(x);
-    // *y = 10;
-
-    // assert_eq!(10, *y);
-    let a = Whatever(3);
-    std::mem::drop(a);
-    let b = Whatever(2);
+    let my_tuple = ("hello", 3, 'a', 2.55, "good");
+    println!("{} {} {}", my_tuple.0, my_tuple.2, my_tuple.3)
 }
