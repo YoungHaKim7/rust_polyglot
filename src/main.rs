22f3a6628b2e8bd9fab47214a54ec6f6ef2af27a
YoungHa Kim <ytok1108@aol.com>
2022-06-01 23:29:58 +0900

    use std::ops::DerefMut;

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::ops::Deref;
+use std::ops::DerefMut;
 
 struct MyBox<T>(T);
 
@@ -16,12 +17,27 @@ impl<T> Deref for MyBox<T> {
     }
     // *y -> *(y.deref())
 }
-fn hello(name: &str) {
-    println!("Hello, {}! ", name);
+
+impl<T> DerefMut for MyBox<T> {
+    fn deref_mut(&mut self) -> &mut T {
+        &mut self.0
+    }
 }
 
+struct Whatever(i32);
+
+impl Drop for Whatever {
+    fn drop(&mut self) {
+        println!("Dropping Whatever with data: {}!", self.0);
+    }
+}
 fn main() {
-    let m = MyBox::new(String::from("Rust "));
-    hello(&(*m)[..]);
-    // hello(&m);
+    // let x = 5;
+    // let mut y = MyBox::new(x);
+    // *y = 10;
+
+    // assert_eq!(10, *y);
+    let a = Whatever(3);
+    std::mem::drop(a);
+    let b = Whatever(2);
 }
