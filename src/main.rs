c2de769557acd49b41996c0f2691f167b02dcf81
YoungHa Kim <ytok1108@aol.com>
2022-06-07 22:48:03 +0900

    Rust Redis__tokio001

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,82 @@
-struct Foo {
-    x: i32,
+use std::{error::Error, time::Duration};
+use tokio::time::sleep;
+
+use redis::{
+    from_redis_value,
+    streams::{StreamRangeReply, StreamReadOptions, StreamReadReply},
+    AsyncCommands, Client,
+};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    // 1) Create Connection
+    let client = Client::open("redis://127.0.0.1/")?;
+    let mut con = client.get_tokio_connection().await?;
+
+    // 2) Set / Get key
+    con.set("my_key", "Hello world!").await?;
+    let result: String = con.get("my_key").await?;
+    println!("-->> my_key: {} \n ", result);
+
+    // 3) xadd to redis stream
+    con.xadd(
+        "my_stream",
+        "*",
+        &[("name", "name-01"), ("title", "title 01")],
+    )
+    .await?;
+    let len: i32 = con.xlen("my-stream").await?;
+    println!("-->> my stream len {}\n", len);
+
+    // 4) xrevrange the read stream
+    let result: Option<StreamRangeReply> = con.xrevrange_count("my_stream", "+", "-", 10).await?;
+    if let Some(reply) = result {
+        for stream_id in reply.ids {
+            println!("-->> xrevrange stream entriy: {:?}", stream_id);
+            for (name, value) in stream_id.map.iter() {
+                println!("  -->>  {}: {}", name, from_redis_value::<String>(value)?);
+            }
+            println!();
+        }
+    }
+
+    // 5) Bloking xread
+    tokio::spawn(async {
+        let client = Client::open("redis://127.0.0.1/").unwrap();
+        let mut con = client.get_tokio_connection().await.unwrap();
+        loop {
+            let opts = StreamReadOptions::default().count(1).block(0);
+            let result: Option<StreamReadReply> = con
+                .xread_options(&["my_stream"], &["$"], &opts)
+                .await
+                .unwrap();
+            if let Some(reply) = result {
+                for stream_key in reply.keys {
+                    println!("-->> xread block: {}", stream_key.key);
+                    for stream_id in stream_key.ids {
+                        println!(" --> streamId: {:?}", stream_id);
+                    }
+                }
+                println!();
+            }
+        }
+    });
+
+    // 6) Add some stream entries
+    sleep(Duration::from_millis(100)).await;
+    con.xadd(
+        "my_strem",
+        "*",
+        &[("name", "name-02"), ("title", "title 02")],
+    )
+    .await?;
+
+    // 7) Final wait & cleanup
+    sleep(Duration::from_millis(1000)).await;
+    con.del("my_key").await?;
+    con.del("my_stream").await?;
+
+    println!("-->> the end");
+
+    Ok(())
 }
-
-fn do_stuff(foo: &mut Foo) {
-    foo.x = foo.x + 1;
-    println!("do stuff: {}", foo.x);
-}
-
-fn main() {
-    let mut foo = Foo { x: 123 };
-    let y = foo.x;
-
-    let foo_b = &mut foo;
-
-    do_stuff(foo_b);
-    do_stuff(&mut foo);
-}
-
