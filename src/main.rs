1d6c2e470c3aa507a47624eee678c475dcab3a9f
YoungHa Kim <ytok1108@aol.com>
2022-06-09 22:57:07 +0900

    Rust by Example__struct001

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,28 @@
-// Formatted print
+// Derive the `fmt::Debug` implementation for `Structure`. `Structure`
+
+// is a stucture which contains a single `i32`.
+#[derive(Debug)]
+struct Structure(i32);
+
+// Put a `Structure` inside of the structure `Deep`. Make it printable
+// also.
+#[derive(Debug)]
+struct Deep(Structure);
+
 fn main() {
-    // In general, the `{}` will be automatically replaced withe and
-    // arguments. These will be stringified.
-    println!("{} days", 31);
-
-    // Without a suffix, 31 becomes an i32. You can change what type 31 is
-    // by providing a suffix. The number 31i64 for example has the type i64
-
-    // There are various optional patterns this works with. Positional
-    // arguments can be used.
-    println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob");
-
-    // As can named arguments.
+    // Printing with `{:?}` is similar to with `{}`.
+    println!("{:?} months in a year.", 12);
     println!(
-        "{subject} {verb} {object}",
-        object = "the lazy dog",
-        subject = "the quick brown fox",
-        verb = "jumps over"
+        "{1:?} {0:?} is the {actor:?} name.",
+        "Slater",
+        "Christian",
+        actor = "actor's"
     );
 
-    // Special formatting can be specified after a `:`.
-    println!(
-        "{} of {:b} people know binary, the other half doesn't ",
-        1, 2
-    );
+    // `Structure` is printable!
+    println!("Now {:?} will print!", Structure(3));
 
-    // You can right-align text with a specified width. This will output
-    // "     1". 5 white space and a "1".
-    println!("{number:>width$}", number = 1, width = 6);
-
-    // You can pad numbers with extra zeros. This will output "000001"
-    println!("{number:0>width$}", number = 1, width = 6);
-
-    // Rust even checks to make sure the correct number of arguments are used.
-    println!("My name is {0}, {1}, {0}", "James", "Bond");
-
-    // Create a structure named `Structure` which contains an `i32`.
-    #[derive(Debug)]
-    struct Structure(i32);
-
-    // However, custom types such as this structure require more complicated
-    // handling. This will not work.
-    println!("This struct `{:?}` :😸 print...", Structure(3));
-
-    // For Rust 1.58 and above, you can directly capture the argument from
-    // surrounding variable, Just like the above, this will output
-    // "    1". 5 white spaces and a "1".
-    let number: f64 = 1.0;
-    let width: usize = 6;
-    println!("{number:>width$}");
+    // The problem with `derive` is there is no control over how
+    // the results look. What if I want this to just show a `7`?
+    println!("Now {:?} will print!", Deep(Structure(7)));
 }
