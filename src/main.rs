e2a119cdc4231cd0249015a8e3fa9d1c983c5c8a
YoungHa Kim <ytok1108@aol.com>
2022-06-11 00:50:05 +0900

    Bevy game engine test__

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,23 @@
-#[derive(Debug)]
-struct Person<'a> {
-    name: &'a str,
-    age: u8,
+use rusty_engine::prelude::*;
+
+struct GameState {
+    health: i32,
+}
+
+fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
+    let player = engine.sprites.get_mut("player").unwrap();
+    player.rotation += std::f32::consts::PI * engine.delta_f32;
+    if player.translation.x > 100.0 {
+        game_state.health -= 1;
+    }
 }
 
 fn main() {
-    let name = "Peter";
-    let age = 27;
-    let peter = Person { name, age };
-
-    // Pretty print
-    println!("{:#?}", peter);
+    let mut game = Game::new();
+    let sprite = game.add_sprite("player", SpritePreset::RacingCarBlue);
+    sprite.scale = 2.0;
+    game.audio_manager.play_music(MusicPreset::Classy8Bit, 1.0);
+    game.add_logic(game_logic);
+    let initial_game_state = GameState { health: 100 };
+    game.run(initial_game_state);
 }
