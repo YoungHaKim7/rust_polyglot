b51d9b1d941fba0ef6fdd4ea0ad8f769b889f2dd
YoungHa Kim <ytok1108@aol.com>
2022-06-11 00:52:31 +0900

    Bevygame_car_shoot.rs

--- src/main.rs
+++ src/main.rs
@@ -1,129 +1,139 @@
 use rand::prelude::*;
 use rusty_engine::prelude::*;
 
-const PLAYER_SPEED: f32 = 250.0;
-const ROAD_SPEED: f32 = 400.0;
-
 struct GameState {
-    health_amount: u8,
-    lost: bool,
+    marble_labels: Vec<String>,
+    cars_left: i32,
+    spawn_timer: Timer,
 }
 
 fn main() {
     let mut game = Game::new();
 
-    // Create the player sprite
-    let player1 = game.add_sprite("player1", SpritePreset::RacingCarBlue);
-    player1.translation.x = -500.0;
-    player1.layer = 10.0;
-    player1.collision = true;
+    // Initial game state
+    let game_state = GameState {
+        marble_labels: vec!["marble1".into(), "marble2".into(), "marble3".into()],
+        cars_left: 25,
+        spawn_timer: Timer::from_seconds(0.0, false),
+    };
 
-    // Start some background music
-    game.audio_manager
-        .play_music(MusicPreset::WhimsicalPopsicle, 0.2);
+    // Set the title of the window
+    game.window_settings(WindowDescriptor {
+        title: "Car Shoot".into(),
+        ..Default::default()
+    });
 
-    // Create the road lines
-    for i in 0..10 {
-        let roadline = game.add_sprite(format!("roadline{}", i), SpritePreset::RacingBarrierWhite);
-        roadline.scale = 0.1;
-        roadline.translation.x = -600.0 + 150.0 * i as f32;
-    }
+    // Start the music
+    game.audio_manager.play_music(MusicPreset::Classy8Bit, 0.1);
 
-    // Create the obstacle sprites
-    let obstacle_presets = vec![
-        SpritePreset::RacingBarrelBlue,
-        SpritePreset::RacingBarrelRed,
-        SpritePreset::RacingBarrelRed,
-        SpritePreset::RacingConeStraight,
-        SpritePreset::RacingConeStraight,
-        SpritePreset::RacingConeStraight,
-        SpritePreset::RollingBlockCorner,
-        SpritePreset::RollingBlockSquare,
-        SpritePreset::RollingBlockSmall,
-    ];
-    for (i, preset) in obstacle_presets.into_iter().enumerate() {
-        let obstacle = game.add_sprite(format!("obstacle{}", i), preset);
-        obstacle.layer = 5.0;
-        obstacle.collision = true;
-        obstacle.translation.x = thread_rng().gen_range(800.0..1600.0);
-        obstacle.translation.y = thread_rng().gen_range(-300.0..300.0);
-    }
+    //
+    let player = game.add_sprite("player", SpritePreset::RacingBarrierRed);
+    player.rotation = UP;
+    player.scale = 0.5;
+    player.translation.y = -325.0;
+    player.layer = 10.0;
 
-    // Create the health message
-    let health_message = game.add_text("health_message", "Health: 5");
-    health_message.translation = Vec2::new(550.0, 320.0);
+    let cars_left = game.add_text("cars left", format!("Cars left: {}", game_state.cars_left));
+    cars_left.translation = Vec2::new(540.0, -320.0);
 
     game.add_logic(game_logic);
-    game.run(GameState {
-        health_amount: 5,
-        lost: false,
-    });
+    game.run(game_state);
 }
 
 fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
-    // Don't run any more game logic if the game has ended
-    if game_state.lost {
-        return;
+    // Handle marble gun movement
+    let player = engine.sprites.get_mut("player").unwrap();
+    if let Some(location) = engine.mouse_state.location() {
+        player.translation.x = location.x;
     }
+    let player_x = player.translation.x;
 
-    // Collect keyboard input
-    let mut direction = 0.0;
-    if engine
-        .keyboard_state
-        .pressed_any(&[KeyCode::Up, KeyCode::W, KeyCode::Comma])
-    {
-        direction += 1.0;
-    }
-    if engine
-        .keyboard_state
-        .pressed_any(&[KeyCode::Down, KeyCode::S, KeyCode::O])
-    {
-        direction -= 1.0;
-    }
-
-    // Move the player sprite
-    let player1 = engine.sprites.get_mut("player1").unwrap();
-    player1.translation.y += direction * PLAYER_SPEED * engine.delta_f32;
-    player1.rotation = direction * 0.15;
-    if player1.translation.y < -360.0 || player1.translation.y > 360.0 {
-        game_state.health_amount = 0;
-    }
-
-    // Move road objects
-    for sprite in engine.sprites.values_mut() {
-        if sprite.label.starts_with("roadline") {
-            sprite.translation.x -= ROAD_SPEED * engine.delta_f32;
-            if sprite.translation.x < -675.0 {
-                sprite.translation.x += 1500.0;
-            }
-        }
-        if sprite.label.starts_with("obstacle") {
-            sprite.translation.x -= ROAD_SPEED * engine.delta_f32;
-            if sprite.translation.x < -800.0 {
-                sprite.translation.x = thread_rng().gen_range(800.0..1600.0);
-                sprite.translation.y = thread_rng().gen_range(-300.0..300.0);
-            }
+    // Shoot marbles!
+    if engine.mouse_state.just_pressed(MouseButton::Left) {
+        if let Some(label) = game_state.marble_labels.pop() {
+            let marble = engine.add_sprite(label, SpritePreset::RollingBallBlue);
+            marble.translation.x = player_x;
+            marble.translation.y = -275.0;
+            marble.layer = 5.0;
+            marble.collision = true;
+            engine.audio_manager.play_sfx(SfxPreset::Impact2, 0.4);
         }
     }
 
-    // Deal with collisions
-    let health_message = engine.texts.get_mut("health_message").unwrap();
+    // Move marbles
+    const MARBLE_SPEED: f32 = 600.0;
+    engine
+        .sprites
+        .values_mut()
+        .filter(|sprite| sprite.label.starts_with("marble"))
+        .for_each(|marble| marble.translation.y += MARBLE_SPEED * engine.delta_f32);
+
+    // Move cars across the screen
+    const CAR_SPEED: f32 = 250.0;
+    engine
+        .sprites
+        .values_mut()
+        .filter(|sprite| sprite.label.starts_with("car"))
+        .for_each(|car| car.translation.x += CAR_SPEED * engine.delta_f32);
+
+    // Clean up sprites that have gone off the screen
+    let mut labels_to_delete = Vec::new();
+    for (label, sprite) in engine.sprites.iter() {
+        if sprite.translation.y > 400.0 || sprite.translation.x > 750.0 {
+            labels_to_delete.push(label.clone());
+        }
+    }
+    for label in labels_to_delete {
+        engine.sprites.remove(&label);
+        if label.starts_with("marble") {
+            game_state.marble_labels.push(label);
+        }
+    }
+
+    // Spawn cars
+    if game_state.spawn_timer.tick(engine.delta).just_finished() {
+        // Reset the timer to a new value
+        game_state.spawn_timer = Timer::from_seconds(thread_rng().gen_range(0.1..1.25), false);
+        // Get the new car
+        if game_state.cars_left > 0 {
+            game_state.cars_left -= 1;
+            let text = engine.texts.get_mut("cars left").unwrap();
+            text.value = format!("Cars left: {}", game_state.cars_left);
+            let label = format!("car{}", game_state.cars_left);
+            use SpritePreset::*;
+            let car_choices = vec![
+                RacingCarBlack,
+                RacingCarBlue,
+                RacingCarGreen,
+                RacingCarRed,
+                RacingCarYellow,
+            ];
+            let sprite_preset = car_choices
+                .iter()
+                .choose(&mut thread_rng())
+                .unwrap()
+                .clone();
+            let car = engine.add_sprite(label, sprite_preset);
+            car.translation.x = -740.0;
+            car.translation.y = thread_rng().gen_range(-100.0..325.0);
+            car.collision = true;
+        }
+    }
+
+    // Handle collisions
     for event in engine.collision_events.drain(..) {
-        // We don't care if obstacles collide with each other or collisions end
-        if !event.pair.either_contains("player1") || event.state.is_end() {
+        if event.state.is_end() {
             continue;
         }
-        if game_state.health_amount > 0 {
-            game_state.health_amount -= 1;
-            health_message.value = format!("Health: {}", game_state.health_amount);
-            engine.audio_manager.play_sfx(SfxPreset::Impact3, 0.5);
+        if !event.pair.one_starts_with("marble") {
+            continue;
         }
-    }
-    if game_state.health_amount == 0 {
-        game_state.lost = true;
-        let game_over = engine.add_text("game over", "Game Over");
-        game_over.font_size = 128.0;
-        engine.audio_manager.stop_music();
-        engine.audio_manager.play_sfx(SfxPreset::Jingle3, 0.5);
+        for label in [event.pair.0, event.pair.1] {
+            engine.sprites.remove(&label);
+            if label.starts_with("marble") {
+                game_state.marble_labels.push(label);
+            }
+            engine.audio_manager.play_sfx(SfxPreset::Confirmation1, 0.2);
+        }
     }
 }
