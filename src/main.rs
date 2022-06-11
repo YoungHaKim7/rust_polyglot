a9c2a50ecb14d0512506213bd06031a866a2523a
YoungHa Kim <ytok1108@aol.com>
2022-06-11 19:15:51 +0900

    Concurruncy Rust_003__sleep

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 use std::thread;
+use std::time::Duration;
 
 fn main() {
-    let handle = thread::spawn(|| {
+    thread::spawn(|| {
         for i in 1..10 {
             println!("Hello from {} thread!", i);
+            thread::sleep(Duration::from_millis(1));
         }
     });
 
     for i in 1..10 {
         println!("hello from my main {} thread ", i);
+        thread::sleep(Duration::from_millis(1));
     }
-
-    handle.join().unwrap();
 }
