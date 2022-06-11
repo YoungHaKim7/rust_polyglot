c9f8ccbd2cda919b972c67a87bb618fc2aa8c1ca
YoungHa Kim <ytok1108@aol.com>
2022-06-11 22:38:18 +0900

    Display fmt::Display __ write__Testcast:List__result: [1, 2, 3]

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,27 @@
-// Display
-use std::fmt; // Import `fmt`
+// Test case : List
 
-// A structure holding two numbers. `Debug` will be derived so the results can
-// be contrasted with `Display`.
-#[derive(Debug)]
-struct MinMax(i64, i64);
+use std::fmt;
 
-// Implement `Display` for `MinMax`.
-impl fmt::Display for MinMax {
-    // Use `self.number` to refer to each positinal data point.
+// Define a structure named `List` containing a `Vec`.
+struct List(Vec<i32>);
+
+impl fmt::Display for List {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "({}, {})", self.0, self.1)
-    }
-}
+        let vec = &self.0;
 
-// Define a structure where the fields are nameable for comparison.
-#[derive(Debug)]
-struct Point2D {
-    x: f64,
-    y: f64,
-}
+        write!(f, "[")?;
 
-// Similarly, implement `Display` for `Point2D`.
-impl fmt::Display for Point2D {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        // Customize so only `x` and `y` are denoted.
-        write!(f, "x: {}, y: {}", self.x, self.y)
+        for (count, v) in vec.iter().enumerate() {
+            if count != 0 {
+                write!(f, ", ")?;
+            }
+            write!(f, "{}", v)?;
+        }
+        write!(f, "]")
     }
 }
 
 fn main() {
-    let minmax = MinMax(0, 14);
-
-    println!("Compare structure");
-    println!("Display: {}", minmax);
-    println!("Debug: {:?}", minmax);
-
-    let big_range = MinMax(-300, 300);
-    let small_range = MinMax(-3, 3);
-
-    println!(
-        "The big range is {big} and the small is {small}",
-        small = small_range,
-        big = big_range
-    );
-
-    let point = Point2D { x: 3.3, y: 7.2 };
-
-    println!("Compare points: ");
-    println!("Display: {}", point);
-    println!("Debug: {:?}", point);
-
-    // Error. Both `Debug` and `Display` were implement, but `{:b}`
-    // requires `fmt::Binary` to be implemented. This will not work.
-    // println!("What does Point2D look like in binary: {:b}?", point);
+    let v = List(vec![1, 2, 3]);
+    println!("{v}");
 }
