e325d61da8f4ba6e56a51006eb34c333af1c692d
YoungHa Kim <ytok1108@aol.com>
2022-06-11 19:14:20 +0900

    Concurruncy Rust_002__join

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use std::thread;
 
 fn main() {
-    thread::spawn(|| {
-        for i in 1..3 {
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
             println!("Hello from {} thread!", i);
         }
     });
 
-    for i in 1..3 {
+    for i in 1..10 {
         println!("hello from my main {} thread ", i);
     }
+
+    handle.join().unwrap();
 }
