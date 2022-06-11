91624155e16614588f71a4af498abdf5c9baa101
YoungHa Kim <ytok1108@aol.com>
2022-06-11 23:02:03 +0900

    Display City__Latitude_Longitude___result: Dublin: 53.348° N 6.260° W Oslo: 59.950° N 10.750° E Vancouver: 49.250° N 123.100° W Color { red: 128, green: 255, blue: 90 } Color { red: 0, green: 3, blue: 254 } Color { red: 0, green: 0, blue: 0 }

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,78 @@
 // Formatting
 
-// 8진수
-use std::fmt;
+use std::fmt::{self, Display, Formatter};
 
-struct Length(i32);
+struct City {
+    name: &'static str,
+    lat: f32,
+    lon: f32,
+}
 
-impl fmt::Octal for Length {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let val = self.0;
+impl Display for City {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        let lat_c = if self.lat >= 0.0 { 'N' } else { 'S' };
+        let lon_c = if self.lon >= 0.0 { 'E' } else { 'W' };
 
-        fmt::Octal::fmt(&val, f) // delegate to i32's implementation
+        write!(
+            f,
+            "{}: {:.3}° {} {:.3}° {}",
+            self.name,
+            self.lat.abs(),
+            lat_c,
+            self.lon.abs(),
+            lon_c
+        )
     }
 }
+
+#[derive(Debug)]
+struct Color {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
+
 fn main() {
-    let l = Length(9);
-    println!("l as octal is: {l:o}");
-    // 0o12  8 진수 표기법
-    // = 9 decimal 10 진수
+    for city in [
+        City {
+            name: "Dublin",
+            lat: 53.347778,
+            lon: -6.259722,
+        },
+        City {
+            name: "Oslo",
+            lat: 59.95,
+            lon: 10.75,
+        },
+        City {
+            name: "Vancouver",
+            lat: 49.25,
+            lon: -123.1,
+        },
+    ]
+    .iter()
+    {
+        println!("{}", *city);
+    }
+    for color in [
+        Color {
+            red: 128,
+            green: 255,
+            blue: 90,
+        },
+        Color {
+            red: 0,
+            green: 3,
+            blue: 254,
+        },
+        Color {
+            red: 0,
+            green: 0,
+            blue: 0,
+        },
+    ]
+    .iter()
+    {
+        println!("{:?}", *color);
+    }
 }
