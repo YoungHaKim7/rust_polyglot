cab14df84528ab202a4d0b6b424e1b67a54911c0
YoungHa Kim <ytok1108@aol.com>
2022-06-14 07:01:19 +0900

    rayon __ count print 2thread

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
-use rand::Rng;
+use rayon::prelude::*;
+use std::time::Instant;
 
 fn main() {
-    let mut rng = rand::thread_rng();
+    let time_1 = Instant::now();
+    let time_2 = Instant::now();
+    let async_vec_1: Vec<_> = (0..100).into_par_iter().collect();
+    let async_vec_2: Vec<_> = (0..100).into_par_iter().collect();
 
-    let vals: Vec<u64> = (0..1000).map(|_| rng.gen_range(0..1000)).collect();
+    for i in async_vec_1.iter() {
+        println!("1 Thread Element : {:?}", i);
+    }
+    let time1_ela = time_1.elapsed();
 
-    println!("{vals:?}");
+    for i in async_vec_2.iter() {
+        println!("2 Thread Element : {:?}", i);
+    }
+    let time2_ela = time_2.elapsed();
+
+    println!("{:?}", time1_ela);
+    println!("{:?}", time2_ela);
 }
