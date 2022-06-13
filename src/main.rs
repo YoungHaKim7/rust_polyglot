100f6f5be49a317d6b3f99cf83dd2d71fb2046d4
YoungHa Kim <ytok1108@aol.com>
2022-06-13 20:55:03 +0900

    Generate Random Values of a custom type

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
 use rand::Rng;
+use rand_distr::{Distribution, Standard};
+
+#[derive(Debug)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Distribution<Point> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Point {
+        let (rand_x, rand_y) = rng.gen();
+        Point {
+            x: rand_x,
+            y: rand_y,
+        }
+    }
+}
 
 fn main() {
     let mut rng = rand::thread_rng();
-
-    let n1: u8 = rng.gen();
-    let n2: u16 = rng.gen();
-    println!("Random u8: {n1}");
-    println!("Random u16: {n2}");
-    println!("Random u32: {}", rng.gen::<u32>());
-    println!("Random i32: {}", rng.gen::<i32>());
-    println!("Random float : {}", rng.gen::<f64>());
+    let rand_tuple = rng.gen::<(i32, bool, f64)>();
+    let rand_point: Point = rng.gen();
+    println!("Random tuple: {:?}", rand_tuple);
+    println!("Random tuple: {:?}", rand_point);
 }
