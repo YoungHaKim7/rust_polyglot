eb965d6cfa3efafe223887d4f36116dd8129dd48
YoungHa Kim <ytok1108@aol.com>
2022-06-14 07:52:33 +0900

    rayon vs normal bench mark test

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,17 @@ use std::time::Instant;
 
 fn main() {
     let time_1 = Instant::now();
+    let async_vec: Vec<_> = (0..1_000_000).into_par_iter().collect();
+    let time_1_ela = time_1.elapsed();
+
     let time_2 = Instant::now();
-    let async_vec_1: Vec<_> = (0..100).into_par_iter().collect();
-    let async_vec_2: Vec<_> = (0..100).into_par_iter().collect();
+    let sync_vec: Vec<_> = (0..1_000_000).into_iter().collect();
+    let time_2_ela = time_2.elapsed();
 
-    for i in async_vec_1.iter() {
-        println!("1 Thread Element : {:?}", i);
-    }
-    let time1_ela = time_1.elapsed();
+    // for i in async_vec.iter() {
+    //     println!("Element : {:?}", i);
+    // }
 
-    for i in async_vec_2.iter() {
-        println!("2 Thread Element : {:?}", i);
-    }
-    let time2_ela = time_2.elapsed();
-
-    println!("{:?}", time1_ela);
-    println!("{:?}", time2_ela);
+    println!("async_vec time__rayon : {:?}", time_1_ela);
+    println!("sync_vec time : {:?}", time_2_ela);
 }
