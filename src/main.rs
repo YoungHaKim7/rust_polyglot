e44bb4b4423768c184913a55e3e685c42d804500
YoungHa Kim <ytok1108@aol.com>
2022-06-15 20:42:32 +0900

    iter vs iter_par_iter() ___rayon

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
-use rayon::iter::Either;
 use rayon::prelude::*;
+use std::time::Instant;
 
 fn main() {
-    let (first, (left, right)): (Vec<_>, (Vec<_>, Vec<_>)) = (0..8)
-        .into_par_iter()
-        .map(|x| {
-            if x % 2 == 0 {
-                (x, Either::Left(x * 4))
-            } else {
-                (-x, Either::Right(x * 3))
-            }
-        })
-        .collect();
-    println!("{first:?}");
-    println!("{left:?}");
-    println!("{right:?}");
+    let time_1 = Instant::now();
+    let sync_vec_1: Vec<_> = (0..100_000_000).into_iter().collect();
+    let time1_ela = time_1.elapsed();
+    let time_2 = Instant::now();
+    let async_vec_2: Vec<_> = (0..100_000_000).into_par_iter().collect();
+    let time2_ela = time_2.elapsed();
+
+    // for i in sync_vec_1.iter() {
+    //     println!("sync Thread Element : {i:?}");
+    // }
+
+    // for i in async_vec_2.iter() {
+    //     println!("async Thread Element : {i:?}");
+    // }
+
+    println!("sync_vec : into_iter() = {:?}", time1_ela);
+    println!("async_vec : into_par_iter() = {:?}", time2_ela);
 }
