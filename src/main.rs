712c411995096d2ed169e2817584a06959c21aec
YoungHa Kim <ytok1108@aol.com>
2022-06-16 06:48:01 +0900

    Hello World wasm-binden ___Guide 001

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,2 @@
-use rayon::prelude::*;
-use std::time::Instant;
+fn main() {}
 
-fn main() {
-    let time_1 = Instant::now();
-    let sync_vec_1: Vec<_> = (0..100_000_000).into_iter().collect();
-    let time1_ela = time_1.elapsed();
-    let time_2 = Instant::now();
-    let async_vec_2: Vec<_> = (0..100_000_000).into_par_iter().collect();
-    let time2_ela = time_2.elapsed();
-
-    // for i in sync_vec_1.iter() {
-    //     println!("sync Thread Element : {i:?}");
-    // }
-
-    // for i in async_vec_2.iter() {
-    //     println!("async Thread Element : {i:?}");
-    // }
-
-    println!("sync_vec : into_iter() = {:?}", time1_ela);
-    println!("async_vec : into_par_iter() = {:?}", time2_ela);
-}
