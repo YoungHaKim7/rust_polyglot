d663b02474a8e15e075e8c81ae784b41ce742b2f
YoungHa Kim <ytok1108@aol.com>
2022-06-17 22:21:45 +0900

    thread::available_parallelism

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,8 @@
-// Sort a vector in parallel
-use rand::thread_rng;
-use rand::Rng;
-use rayon::prelude::*;
-use std::time::Instant;
+use std::{io, thread};
 
-fn main() {
-    let mut rng = thread_rng();
-    let mut v: Vec<u64> = (0..20_000_000)
-        .map(|_| rng.gen_range(0..20_000_000))
-        .collect();
-    let time_1 = Instant::now();
-    v.par_sort_unstable();
-    let time_1_ela = time_1.elapsed();
-    println!("par_sort time : {:?}", time_1_ela);
-
-    let mut v1: Vec<u64> = (0..20_000_000)
-        .map(|_| rng.gen_range(0..20_000_000))
-        .collect();
-    let time_2 = Instant::now();
-    v1.sort();
-    let time_2_ela = time_2.elapsed();
-    println!("sort time : {:?}", time_2_ela);
+fn main() -> io::Result<()> {
+    let count = thread::available_parallelism()?.get();
+    println!("available_parallelism cpu no. {}", count);
+    Ok(())
 }
+
