2dfb46eb7e50233595e6abd6f145f9b21cd363fd
YoungHa Kim <ytok1108@aol.com>
2022-06-19 00:23:50 +0900

    Draw fractal dispatching worlk to a thread pool

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,98 @@
-// Calculate SHA256 sum of iso files concurrently
-use ring::digest::{Context, Digest, SHA256};
-use std::fs::File;
-use std::io::{BufReader, Error, Read};
-use std::path::Path;
-use std::sync::mpsc::channel;
+#[macro_use]
+// extern crate error_chain;
+extern crate image;
+extern crate num;
+extern crate threadpool;
+// Draw fractal dispatching work to a thread pool
+
+use image::{ImageBuffer, Pixel, Rgb};
+use num::complex::Complex;
+use std::sync::mpsc::{channel, RecvError};
 use threadpool::ThreadPool;
-use walkdir::WalkDir;
 
-fn compute_digest<P: AsRef<Path>>(filepath: P) -> Result<(Digest, P), Error> {
-    let mut buf_reader = BufReader::new(File::open(&filepath)?);
-    let mut context = Context::new(&SHA256);
-    let mut buffer = [0; 1024];
+// error_chain! {
+//     foreign_links {
+//         MpscRecv(RecvError);
+//         Io(std::io::Error);
+//     }
+// }
 
-    loop {
-        let count = buf_reader.read(&mut buffer)?;
-        if count == 0 {
-            break;
-        }
-        context.update(&buffer[..count]);
-    }
+fn wavelength_to_rgb(wavelength: u32) -> Rgb<u8> {
+    let wave = wavelength as f32;
 
-    Ok((context.finish(), filepath))
+    let (r, g, b) = match wavelength {
+        380..=439 => ((440. - wave) / (440. - 380.), 0.0, 1.0),
+        440..=489 => (0.0, (wave - 440.) / (490. - 440.), 1.0),
+        490..=509 => (0.0, 1.0, (510. - wave) / (510. - 490.)),
+        510..=579 => ((wave - 510.) / (580. - 510.), 1.0, 0.0),
+        580..=644 => (1.0, (645. - wave) / (645. - 580.), 0.0),
+        645..=780 => (1.0, 0.0, 0.0),
+        _ => (0.0, 0.0, 0.0),
+    };
+
+    let factor = match wavelength {
+        380..=419 => 0.3 + 0.7 * (wave - 380.) / (420. - 380.),
+        701..=780 => 0.3 + 0.7 * (780. - wave) / (780. - 700.),
+        _ => 1.0,
+    };
+
+    let (r, g, b) = (
+        normalize(r, factor),
+        normalize(g, factor),
+        normalize(b, factor),
+    );
+    Rgb::from_channels(r, g, b, 0)
 }
 
-fn main() -> Result<(), Error> {
-    let pool = ThreadPool::new(4);
+fn julia(c: Complex<f32>, x: u32, y: u32, width: u32, height: u32, max_iter: u32) -> u32 {
+    let width = width as f32;
+    let height = height as f32;
 
+    let mut z = Complex {
+        re: 3.0 * (x as f32 - 0.5 * width) / width,
+        im: 2.0 * (y as f32 - 0.5 * height) / height,
+    };
+
+    let mut i = 0;
+    for t in 0..max_iter {
+        if z.norm() >= 2.0 {
+            break;
+        }
+        z = z * z + c;
+        i = t;
+    }
+    i
+}
+
+fn normalize(color: f32, factor: f32) -> u8 {
+    ((color * factor).powf(0.8) * 255.) as u8
+}
+
+fn main() -> Result<(), RecvError> {
+    let (width, height) = (1920, 1080);
+    let mut img = ImageBuffer::new(width, height);
+    let iterations = 300;
+
+    let c = Complex::new(-0.8, 0.156);
+
+    let pool = ThreadPool::new(4);
     let (tx, rx) = channel();
 
-    for entry in WalkDir::new("/Users/globalyoung/Desktop/")
-        .follow_links(true)
-        .into_iter()
-        .filter_map(|e| e.ok())
-        .filter(|e| !e.path().is_dir())
-    {
-        let path = entry.path().to_owned();
+    for y in 0..height {
         let tx = tx.clone();
         pool.execute(move || {
-            let digest = compute_digest(path);
-            tx.send(digest).expect("Could not send data!");
+            for x in 0..width {
+                let i = julia(c, x, y, width, height, iterations);
+                let pixel = wavelength_to_rgb(380 + i * 400 / iterations);
+                tx.send((x, y, pixel)).expect("Could not send data!");
+            }
         });
     }
-    drop(tx);
-    for t in rx.iter() {
-        let (sha, path) = t?;
-        println!("{:?} {:?}", sha, path)
+
+    for _ in 0..(width * height) {
+        let (x, y, pixel) = rx.recv()?;
+        img.put_pixel(x, y, pixel);
     }
+    let _ = img.save("output.png");
     Ok(())
 }
