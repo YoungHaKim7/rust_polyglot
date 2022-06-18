75d5bdadb2a3313313d29ef30099c0bb9607eeda
YoungHa Kim <ytok1108@aol.com>
2022-06-18 20:09:14 +0900

    Maintain global mutable state__result: 0 : [apple, orange, peach]

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,49 @@
-// Pass data between two threads
-use crossbeam;
-use crossbeam_channel::unbounded;
-use std::{thread, time};
+// Maintain global mutable state
+use lazy_static::lazy_static;
+use std::error::Error;
+use std::fmt;
+use std::sync::Mutex;
 
-fn main() {
-    let (snd, rcv) = unbounded();
-    let n_msgs = 5;
-    crossbeam::scope(|s| {
-        s.spawn(|_| {
-            for i in 0..n_msgs {
-                snd.send(i).unwrap();
-                thread::sleep(time::Duration::from_millis(100));
-            }
-        });
-    })
-    .unwrap();
-    for _ in 0..n_msgs {
-        let msg = rcv.recv().unwrap();
-        println!("Received {msg}");
+#[derive(Debug)]
+struct MyError {
+    details: String,
+}
+
+impl MyError {
+    fn new(msg: &str) -> MyError {
+        MyError {
+            details: msg.to_string(),
+        }
     }
+}
+
+impl fmt::Display for MyError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.details)
+    }
+}
+
+lazy_static! {
+    static ref FRUIT: Mutex<Vec<String>> = Mutex::new(Vec::new());
+}
+
+fn insert(fruit: &str) -> Result<(), Box<dyn Error>> {
+    let mut db = FRUIT.lock().map_err(|_| "Failed to acquire MutexGuard")?;
+    db.push(fruit.to_string());
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    insert("apple")?;
+    insert("orange")?;
+    insert("peach")?;
+    {
+        let db = FRUIT.lock().map_err(|_| "Failed to acquire MutexGuard");
+
+        db.iter()
+            .enumerate()
+            .for_each(|(i, item)| println!("{} : {:?}", i, item));
+    }
+    // insert("grape").to_string()?;
+    Ok(())
 }
