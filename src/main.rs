cf418dde1fb07fe9afde08c1f3871dc2bdf68bd6
YoungHa Kim <ytok1108@aol.com>
2022-06-23 07:11:34 +0900

    With child drawing area, we can easily render multi-panel figures. Here's an example for making multi-panel figures:

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,43 @@
 // https://plotters-rs.github.io/book/basic/basic_data_plotting.html
+// With child drawing area, we can easily render multi-panel figures. Here's an example for making multi-panel figures:
 use plotters::prelude::*;
 
 fn main() {
-    let root_area = BitMapBackend::new("images/2.5.png", (600, 400)).into_drawing_area();
-    root_area.fill(&WHITE).unwrap();
+    let root = BitMapBackend::new("images/multi-panel-figure.png", (640, 480)).into_drawing_area();
+    root.fill(&WHITE).unwrap();
+    let (upper, lower) = root.split_vertically((80).percent());
 
-    let mut ctx = ChartBuilder::on(&root_area)
-        .set_label_area_size(LabelAreaPosition::Left, 40)
-        .set_label_area_size(LabelAreaPosition::Bottom, 40)
-        .caption("Line Plot Demo", ("sans-serif", 40))
-        .build_cartesian_2d(-10..10, 0..100)
+    let mut upper_chart = ChartBuilder::on(&upper)
+        .set_label_area_size(LabelAreaPosition::Left, 30)
+        .set_label_area_size(LabelAreaPosition::Right, 30)
+        .set_label_area_size(LabelAreaPosition::Top, 30)
+        .build_cartesian_2d(0.0..10.0, -1.0..1.0)
+        .unwrap();
+    upper_chart.configure_mesh().draw().unwrap();
+
+    upper_chart
+        .draw_series(LineSeries::new(
+            (0..100).map(|x| x as f64 / 10.0).map(|x| (x, x.sin())),
+            &BLACK,
+        ))
         .unwrap();
 
-    ctx.configure_mesh().draw().unwrap();
+    let mut lower_chart = ChartBuilder::on(&lower)
+        .set_label_area_size(LabelAreaPosition::Left, 30)
+        .set_label_area_size(LabelAreaPosition::Right, 30)
+        .build_cartesian_2d(0.0..10.0, -1.0..1.0)
+        .unwrap();
+    lower_chart.configure_mesh().draw().unwrap();
 
-    ctx.draw_series(LineSeries::new((-10..=10).map(|x| (x, x * x)), &GREEN))
+    lower_chart
+        .draw_series((0..100).map(|x| x as f64 / 10.0).map(|x| {
+            let color = if x.cos() > 0.0 {
+                RED.mix(0.3).filled()
+            } else {
+                GREEN.mix(0.3).filled()
+            };
+            Rectangle::new([(x, 0.0), (x + 0.1, x.cos())], color)
+        }))
         .unwrap();
 }
 
