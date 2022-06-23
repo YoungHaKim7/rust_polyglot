607d8dbdd1b95cd791be255e65a0b957dc621560
YoungHa Kim <ytok1108@aol.com>
2022-06-23 21:05:37 +0900

    sine cos graphic

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,32 @@
 use plotters::prelude::*;
 
 fn main() {
-    let root_area = BitMapBackend::new("images/111.png", (600, 400)).into_drawing_area();
+    let root_area = BitMapBackend::new("images/2222.png", (1200, 800)).into_drawing_area();
     root_area.fill(&WHITE).unwrap();
 
     let mut ctx = ChartBuilder::on(&root_area)
         .set_label_area_size(LabelAreaPosition::Left, 40)
         .set_label_area_size(LabelAreaPosition::Bottom, 40)
-        .caption("Line Plot Demo", ("sans-serif", 40))
-        .build_cartesian_2d(-10..10, 0..100)
+        .caption("Legend", ("sans-serif", 40))
+        .build_cartesian_2d(-4.0..4.0, -1.2..1.2)
         .unwrap();
 
     ctx.configure_mesh().draw().unwrap();
 
-    ctx.draw_series(LineSeries::new((-10..=10).map(|x| (x, x * x)), &GREEN))
+    let x_kps: Vec<_> = (-80..80).map(|x| x as f64 / 20.0).collect();
+    ctx.draw_series(LineSeries::new(x_kps.iter().map(|x| (*x, x.sin())), &RED))
+        .unwrap()
+        .label("Sine")
+        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
+
+    ctx.draw_series(LineSeries::new(x_kps.iter().map(|x| (*x, x.cos())), &BLUE))
+        .unwrap()
+        .label("Cosine")
+        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &BLUE));
+
+    ctx.configure_series_labels()
+        .border_style(&BLACK)
+        .background_style(&WHITE.mix(0.8))
+        .draw()
         .unwrap();
 }
