8930332fe2cbe5c479c2ab471bc65c776fdd164b
YoungHa Kim <ytok1108@aol.com>
2022-06-23 20:51:34 +0900

    x*x*x__function__result: [(-10, -1000), (-9, -729), (-8, -512), (-7, -343), (-6, -216), (-5, -125), (-4, -64), (-3, -27), (-2, -8), (-1, -1), (0, 0), (1, 1), (2, 8), (3, 27), (4, 64), (5, 125), (6, 216), (7, 343), (8, 512), (9, 729), (10, 1000)]

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,9 @@
-// https://plotters-rs.github.io/book/basic/basic_data_plotting.html
-// Time Series Chart
-use plotters::prelude::*;
-use chrono::{Utc, TimeZone};
+fn y_xxx(x: i32) -> i32 {
+    x * x * x
+}
 
 fn main() {
-  let root_area = BitMapBackend::new("images/2.11.png", (600, 400))
-    .into_drawing_area();
-  root_area.fill(&WHITE).unwrap();
+    let my_vec = ((-10..=10).map(|x| (x, y_xxx(x)))).collect::<Vec<_>>();
 
-  let start_date = Utc.ymd(2019, 10, 1);
-  let end_date = Utc.ymd(2019, 10, 18);
-
-    let mut ctx = ChartBuilder::on(&root_area)
-        .set_label_area_size(LabelAreaPosition::Left, 40)
-        .set_label_area_size(LabelAreaPosition::Bottom, 40)
-        .caption("MSFT daily close price", ("sans-serif", 40))
-        .build_cartesian_2d(start_date..end_date, 130.0..145.0)
-        .unwrap();
-
-    ctx.configure_mesh().draw().unwrap();
-
-    ctx.draw_series(
-        LineSeries::new(
-            (0..).zip(DATA.iter()).map(|(idx, price)| {
-                let day = (idx / 5) * 7 + idx % 5 + 1;
-                let date = Utc.ymd(2019,10, day);
-                (date, *price)
-            }),
-            &BLUE,
-        )
-    ).unwrap();
-
+    println!("{my_vec:?}");
 }
-const DATA: [f64; 14] = [ 137.24, 136.37, 138.43, 137.41, 139.69, 140.41, 141.58, 139.55, 139.68, 139.10, 138.24, 135.67, 137.12, 138.12];
