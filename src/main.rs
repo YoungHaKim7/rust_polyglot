5b3618706dfd99364b0352dedc83b295a1d5815e
YoungHa Kim <ytok1108@aol.com>
2022-06-23 20:44:42 +0900

    R Lang sin cos sin*cos & function())

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,35 @@
 // https://plotters-rs.github.io/book/basic/basic_data_plotting.html
-// With child drawing area, we can easily render multi-panel figures. Here's an example for making multi-panel figures:
+// Time Series Chart
 use plotters::prelude::*;
+use chrono::{Utc, TimeZone};
 
 fn main() {
-    let root = BitMapBackend::new("images/multi-panel-figure.png", (640, 480)).into_drawing_area();
-    root.fill(&WHITE).unwrap();
-    let (upper, lower) = root.split_vertically((80).percent());
+  let root_area = BitMapBackend::new("images/2.11.png", (600, 400))
+    .into_drawing_area();
+  root_area.fill(&WHITE).unwrap();
 
-    let mut upper_chart = ChartBuilder::on(&upper)
-        .set_label_area_size(LabelAreaPosition::Left, 30)
-        .set_label_area_size(LabelAreaPosition::Right, 30)
-        .set_label_area_size(LabelAreaPosition::Top, 30)
-        .build_cartesian_2d(0.0..10.0, -1.0..1.0)
-        .unwrap();
-    upper_chart.configure_mesh().draw().unwrap();
+  let start_date = Utc.ymd(2019, 10, 1);
+  let end_date = Utc.ymd(2019, 10, 18);
 
-    upper_chart
-        .draw_series(LineSeries::new(
-            (0..100).map(|x| x as f64 / 10.0).map(|x| (x, x.sin())),
-            &BLACK,
-        ))
+    let mut ctx = ChartBuilder::on(&root_area)
+        .set_label_area_size(LabelAreaPosition::Left, 40)
+        .set_label_area_size(LabelAreaPosition::Bottom, 40)
+        .caption("MSFT daily close price", ("sans-serif", 40))
+        .build_cartesian_2d(start_date..end_date, 130.0..145.0)
         .unwrap();
 
-    let mut lower_chart = ChartBuilder::on(&lower)
-        .set_label_area_size(LabelAreaPosition::Left, 30)
-        .set_label_area_size(LabelAreaPosition::Right, 30)
-        .build_cartesian_2d(0.0..10.0, -1.0..1.0)
-        .unwrap();
-    lower_chart.configure_mesh().draw().unwrap();
+    ctx.configure_mesh().draw().unwrap();
 
-    lower_chart
-        .draw_series((0..100).map(|x| x as f64 / 10.0).map(|x| {
-            let color = if x.cos() > 0.0 {
-                RED.mix(0.3).filled()
-            } else {
-                GREEN.mix(0.3).filled()
-            };
-            Rectangle::new([(x, 0.0), (x + 0.1, x.cos())], color)
-        }))
-        .unwrap();
+    ctx.draw_series(
+        LineSeries::new(
+            (0..).zip(DATA.iter()).map(|(idx, price)| {
+                let day = (idx / 5) * 7 + idx % 5 + 1;
+                let date = Utc.ymd(2019,10, day);
+                (date, *price)
+            }),
+            &BLUE,
+        )
+    ).unwrap();
+
 }
-
+const DATA: [f64; 14] = [ 137.24, 136.37, 138.43, 137.41, 139.69, 140.41, 141.58, 139.55, 139.68, 139.10, 138.24, 135.67, 137.12, 138.12];
