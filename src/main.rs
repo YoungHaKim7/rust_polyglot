f159c287b3fa9ac3f27b2f0baa796e86006a6a88
YoungHa Kim <ytok1108@aol.com>
2022-06-25 23:19:46 +0900

    error float crate____rust

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,40 @@
 // https://lkarev.medium.com/calculating-30000-pi-digits-in-10-seconds-using-multi-threaded-programming-cc417d00a217
-use factorial::Factorial;
+
+use rug::ops::Pow;
+use rug::Float;
+use std::ops::Div;
+use std::result;
 
 fn main() {
-    let mut sum: f64 = 0.0;
+    pub static DEFAULT_PRECISION: u32 = 10_000;
+
+    let mut sum: Float = Float::with_val(DEFAULT_PRECISION, 0.0);
     let max_elements: u32 = 10;
 
     for n in 0..max_elements {
-        let first_fraction: f64 =
-            (4 * n).factorial() as f64 / ((4_u32.pow(n) * n.factorial()).pow(4)) as f64;
+        let first_fraction_num: Float = Float::with_val(DEFAULT_PRECISION, Float::factorial(4 * n));
+        let first_fraction_denom: Float = Float::with_val(
+            DEFAULT_PRECISION,
+            Float::with_val(DEFAULT_PRECISION, 4).pow(n)
+                * Float::with_val(DEFAULT_PRECISION, Float::factorial(n)),
+        )
+        .pow(4);
 
-        let second_fraction: f64 = (23690 * n + 1103) as f64 / (99_f64.powf((4 * n) as f64));
+        let second_fraction_num: Float = Float::with_val(DEFAULT_PRECISION, 23690 * n + 1103);
+        let second_fraction_denom: Float = Float::with_val(
+            DEFAULT_PRECISION,
+            Float::with_val(DEFAULT_PRECISION, 99).pow(4 * n),
+        );
+        let first_fraction: Float = first_fraction_num / first_fraction_denom;
+        let second_fraction: Float = second_fraction_num / second_fraction_denom;
 
-        let sum_element: f64 = first_fraction * second_fraction;
-
+        let sum_element: FLoat = first_fraction * second_fraction;
         sum += sum_element;
     }
 
-    let first_const: f64 = 8_f64.sqrt() / (99_f64.powf(2_f64));
+    let first_const: Float = Float::with_val(DEFAULT_PRECISION, 8).sqrt()
+        / Float::with_val(DEFAULT_PRECISION, 99).pow(2);
+    let result = Float::with_val(DEFAULT_PRECISION, 1) / (first_const * sum);
 
-    let result: f64 = 1_f64 / (first_const * sum);
-
-    println!("Pi is {} ", result);
+    println!("Pi is {}", result);
 }
