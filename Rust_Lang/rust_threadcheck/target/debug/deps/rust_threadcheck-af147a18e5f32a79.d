8be4328ca0b1a69e793fba2d871f8a2db84d2ebc
YoungHa Kim <ytok1108@aol.com>
2022-07-01 22:53:49 +0900

    rust thread check & cpp __open mp hello world

--- Rust_Lang/rust_threadcheck/target/debug/deps/rust_threadcheck-af147a18e5f32a79.d
+++
@@ -1,5 +0,0 @@
-/Users/globalyoung/Documents/Project/Github/rust_project/rust_polyglot/Rust_Lang/rust_threadcheck/target/debug/deps/rust_threadcheck-af147a18e5f32a79: src/main.rs
-
-/Users/globalyoung/Documents/Project/Github/rust_project/rust_polyglot/Rust_Lang/rust_threadcheck/target/debug/deps/rust_threadcheck-af147a18e5f32a79.d: src/main.rs
-
-src/main.rs:
--- Rust_Lang/rust_threadcheck/target/debug/deps/rust_threadcheck-b50324d9262ff581.d
+++
@@ -1,5 +0,0 @@
-/Users/globalyoung/Documents/Project/Github/rust_project/rust_polyglot/Rust_Lang/rust_threadcheck/target/debug/deps/rust_threadcheck-b50324d9262ff581.rmeta: src/main.rs
-
-/Users/globalyoung/Documents/Project/Github/rust_project/rust_polyglot/Rust_Lang/rust_threadcheck/target/debug/deps/rust_threadcheck-b50324d9262ff581.d: src/main.rs
-
-src/main.rs:
