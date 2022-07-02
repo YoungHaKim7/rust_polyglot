c33947e3821c665ff91e53017cd61fecd2215349
YoungHa Kim <ytok1108@aol.com>
2022-07-02 17:41:26 +0900

    asm sample assembly

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::arch::asm;
+fn do_nothing() {
+    #![feature(asm)]
+    unsafe {
+        asm!("NOP");
+    }
+
+    // asm!("NOP");
+    // That would be invalid here, because we are no longer in an
+    // unsafe block.
+}
+
 fn main() {
-    println!("Hello world!");
+    do_nothing();
 }
