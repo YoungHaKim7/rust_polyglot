ca35b0ec4694a3790673ea6eb8bf0d566f4a1a5a
YoungHa Kim <ytok1108@aol.com>
2022-07-12 22:06:50 +0900

    zig 04_array [0]

---
+++ zig_lang/exercises/04_arrays.zig
@@ -0,0 +1,11 @@
+const std = @import("std");
+
+pub fn main() void {
+    var some_primes = [_]u8{ 1, 3, 5, 7, 11, 13, 17, 19 };
+
+    some_primes[0] = 2;
+
+    const first = some_primes[0];
+
+    std.debug.print("Fisrt: {}", .{first});
+}
