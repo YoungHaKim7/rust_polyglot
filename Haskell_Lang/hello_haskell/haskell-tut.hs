f22abc3a0fa4f28915af3c39efb809b066abd18c
YoungHa Kim <ytok1108@aol.com>
2022-07-15 21:43:36 +0900

    Haskell hello world

---
+++ Haskell_Lang/hello_haskell/haskell-tut.hs
@@ -0,0 +1,33 @@
+import Data.List
+import System.IO
+
+sumOfNums = sum [1..1000]
+
+-- Int -2^63 2^63
+minInt = minBound :: Int
+
+-- Double
+bigFloat = 3.9999999999999 + 0.00000000000005
+
+always5 :: Int
+always5 = 5
+
+-- comment  one line
+
+{-
+Comment
+
+-}
+
+addEx = 5 + 4
+subEx = 5 - 4
+multEx = 5 * 4
+divEx = 5 / 4
+
+modEx = mod 5 4
+modEx2 = 5 `mod` 4
+
+negNumEx = 5 + (-4)
+
+hello = "hello hakell world"
+
