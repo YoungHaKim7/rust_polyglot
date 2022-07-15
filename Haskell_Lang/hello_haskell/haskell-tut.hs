14e9ca9b3dbaf98ffa07da8cee3a60fa42f7ffa0
YoungHa Kim <ytok1108@aol.com>
2022-07-15 22:02:08 +0900

    ghc --make haskell-tut ./haskell-tut

--- Haskell_Lang/hello_haskell/haskell-tut.hs
+++ Haskell_Lang/hello_haskell/haskell-tut.hs
@@ -1,33 +1,10 @@
+-- ghc --make haskell-tut
+-- ./haskell-tut
+
 import Data.List
 import System.IO
 
-sumOfNums = sum [1..1000]
-
--- Int -2^63 2^63
-minInt = minBound :: Int
-
--- Double
-bigFloat = 3.9999999999999 + 0.00000000000005
-
-always5 :: Int
-always5 = 5
-
--- comment  one line
-
-{-
-Comment
-
--}
-
-addEx = 5 + 4
-subEx = 5 - 4
-multEx = 5 * 4
-divEx = 5 / 4
-
-modEx = mod 5 4
-modEx2 = 5 `mod` 4
-
-negNumEx = 5 + (-4)
-
-hello = "hello hakell world"
-
+main = do 
+    putStrLn "What's your name"
+    name <- getLine
+    putStrLn ("Hello " ++ name)
