af15e62933519b702ea27df9d89132e81c65e46f
YoungHa Kim <ytok1108@aol.com>
2022-07-16 18:57:22 +0900

    clojure Tutorial

---
+++ Clojure_Lang/hello_clojure/tutorial/src/tutorial/core.clj
@@ -0,0 +1,17 @@
+(ns tutorial.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+
+  (def aString "Hello")
+  (def aDouble 1.234)
+  (def aLong 15)
+  ; (format "This is string %s" aString)
+  (format "5 space and %5d" aLong)
+  ; (format "Leading zeros %04d" aLong)
+  ; (format "%-4d left justified" aLong)
+  ; (format "3 decimals %.3f" aDouble)
+  )
+
