5929726d463ff377448a02b179b40047fe00ce4e
YoungHa Kim <ytok1108@aol.com>
2022-07-17 00:33:45 +0900

    clojure ___ lein repl

--- Clojure_Lang/hello_clojure/tutorial/src/tutorial/core.clj
+++ Clojure_Lang/hello_clojure/tutorial/src/tutorial/core.clj
@@ -1,17 +1,19 @@
 (ns tutorial.core
   (:gen-class))
 
+(float? 15)
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
 
-  (def aString "Hello")
+  ; (def aString "Hello")
   (def aDouble 1.234)
-  (def aLong 15)
-  ; (format "This is string %s" aString)
-  (format "5 space and %5d" aLong)
+  ; (def aLong 15)
+  ; (format "this is string %s" aString)
+  ; (format "5 space and %5d" aLong)
+
   ; (format "Leading zeros %04d" aLong)
   ; (format "%-4d left justified" aLong)
-  ; (format "3 decimals %.3f" aDouble)
-  )
+  (format "3 decimals %.3f" aDouble))
 
