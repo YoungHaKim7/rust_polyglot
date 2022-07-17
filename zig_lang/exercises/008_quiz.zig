90918866f6f03e46c5f8b7a284c0f44d9a13f222
YoungHa Kim <ytok1108@aol.com>
2022-07-17 21:53:17 +0900

    quiz2 __ result : letters : YZhifg Program in Zig

--- zig_lang/exercises/008_quiz.zig
+++ zig_lang/exercises/008_quiz.zig
@@ -4,11 +4,17 @@ const print = std.debug.print;
 pub fn main() void {
     const letters = "YZhifg";
 
-    const x: usize = 1;
+    var x: usize = 1;
 
     var lang: [3]u8 = undefined;
 
     lang[0] = letters[x];
+
+    x = 3;
+    lang[1] = letters[x];
+
+    x = 5;
+    lang[2] = letters[x];
 
     print("letters : {s}\n", .{letters});
     print("Program in {s}!\n", .{lang});
