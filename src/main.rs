0eb4287edc673f3d385b3dadbf0c562e656f78a8
YoungHa Kim <ytok1108@aol.com>
2022-08-12 20:20:57 +0900

    rust 1.63 Mutex global variable

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,17 @@
+// use std::sync::Arc;
+
+use std::sync::Mutex;
+
+static S: Mutex<String> = Mutex::new(String::new());
+
+fn do_a_call() {
+    let h = "hello";
+    S.lock().unwrap().push_str(h);
+    println!("{S:?}");
+}
+
 fn main() {
-    let mut a = vec![1, 2, 3];
-    let mut x = 0;
-
-    std::thread::scope(|s| {
-        s.spawn(|| {
-            println!("hello from the first scoped thread");
-            dbg!(&a);
-        });
-        s.spawn(|| {
-            println!("hello from the second scoped thread");
-            x += a[0] + a[2];
-        });
-        println!("hello from the main thread");
-    });
-
-    a.push(4);
-    assert_eq!(x, a.len());
+    do_a_call();
+    do_a_call();
+    do_a_call();
 }
