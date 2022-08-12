844b7cd7eb5e809021c7e62a64c731316c52c84b
YoungHa Kim <ytok1108@aol.com>
2022-08-12 19:51:01 +0900

    rust 1.63 scope

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
-// https://riptutorial.com/rust/example/24222/displaying-raw-pointers
-use std::ptr;
-
-// Create some data, a raw pointer pointing to it and a null pointer
 fn main() {
-    let data: u32 = 42;
-    let raw_ptr = &data as *const u32;
-    let null_ptr = ptr::null() as *const u32;
+    let mut a = vec![1, 2, 3];
+    let mut x = 0;
 
-    // the {:p} mapping shows pointer values as hexadecimal memory addresses
-    println!("Data address: {:p}", &data);
-    println!("Raw pointer address: {:p}", raw_ptr);
-    println!("Null pointer address: {:p}", null_ptr);
+    std::thread::scope(|s| {
+        s.spawn(|| {
+            println!("hello from the first scoped thread");
+            dbg!(&a);
+        });
+        s.spawn(|| {
+            println!("hello from the second scoped thread");
+            x += a[0] + a[2];
+        });
+        println!("hello from the main thread");
+    });
+
+    a.push(4);
+    assert_eq!(x, a.len());
 }
