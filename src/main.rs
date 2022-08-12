38bc820fc0b08fbedfa4cd38faa72d512420aa0a
YoungHa Kim <ytok1108@aol.com>
2022-08-12 21:04:14 +0900

    rust 1.63 ___ std::str::from_utf8(&my_vec).unwrap();___result: Hello , world

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
+use std::collections::VecDeque;
+use std::io::{Read, Write};
+
 fn main() {
-    let array: [usize; 7] = std::array::from_fn(|i| 100 * i);
-    println!("{array:?}");
+    let mut buf: VecDeque<u8> = VecDeque::new();
+    buf.write_all(b"Hello");
+    write!(buf, " , {}!", "world");
+    let ptr = buf.as_slices().0.as_ptr();
+    let my_vec = Vec::from(buf);
+    let my_vec_string = std::str::from_utf8(&my_vec).unwrap();
+
+    println!("{my_vec_string:?}");
 }
