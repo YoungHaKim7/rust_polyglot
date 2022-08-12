ff90f7d356e3c00811363acac35169bb99e40d0a
YoungHa Kim <ytok1108@aol.com>
2022-08-12 21:11:47 +0900

    rust 1.63 ___ fn clone_into()__result: hello [1, 2]

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
-use std::collections::VecDeque;
-use std::io::{Read, Write};
-
 fn main() {
-    let mut buf: VecDeque<u8> = VecDeque::new();
-    buf.write_all(b"Hello");
-    write!(buf, " , {}!", "world");
-    let ptr = buf.as_slices().0.as_ptr();
-    let my_vec = Vec::from(buf);
-    let my_vec_string = std::str::from_utf8(&my_vec).unwrap();
+    let mut s: String = String::new();
+    "hello".clone_into(&mut s);
 
-    println!("{my_vec_string:?}");
+    println!("{s}");
+
+    let mut v: Vec<i32> = Vec::new();
+    [1, 2][..].clone_into(&mut v);
+    println!("{v:?}");
 }
