8c2c05b5cc39d08c39567e83ff20da308f4e7634
YoungHa Kim <ytok1108@aol.com>
2022-08-12 20:34:30 +0900

    rust 1.63 ___ std::array::from_fn___result: [0, 100, 200, 300, 400, 500, 600]

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,4 @@
-use std::os::unix::io::{BorrowedFd, OwnedFd};
-
-extern "C" {
-    fn close(fd: OwnedFd) -> i32;
-
-    fn ftruncate(fd: BorrowedFd, size: i64) -> i32;
-}
-
-// windows
-use std::os::windows::io::{BorrowedHandle, OwnedHandle};
-
-extern "C" {
-    fn CloseHandle(handle: OwnedHandle) -> bool;
-
-    fn SetEndOfFile(handle: BorrowedHandle) -> bool;
-}
-
 fn main() {
-    todo!();
+    let array: [usize; 7] = std::array::from_fn(|i| 100 * i);
+    println!("{array:?}");
 }
