51fc75b64d74660690ca4753d9662366cbe3c6e5
YoungHa Kim <ytok1108@aol.com>
2022-08-13 18:48:01 +0900

    Hashmap_mutex_arc__example__result: k: key-2 v: value-2 k: key-0 v: value-0 k: key-1 v: value-1

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,33 @@
-#[derive(Debug)]
-pub enum Thing {
-    A,
-    B,
+// Thanks: https://stackoverflow.com/a/53894298/665869
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+use std::thread;
+
+type Map = HashMap<String, String>;
+
+fn handle_n_times(count: i32, arc_map: Arc<Mutex<Map>>) {
+    for i in 0..count {
+        let clone_arc = arc_map.clone();
+        thread::spawn(move || {
+            let mut map = clone_arc.lock().unwrap();
+            map.insert(format!("key-{}", i), format!("value-{}", i));
+        });
+    }
 }
 
-impl Drop for Thing {
-    fn drop(&mut self) {}
+fn print_map_items(arc_map: Arc<Mutex<Map>>) {
+    let clone_arc = arc_map.clone();
+    let map = clone_arc.lock().unwrap();
+    for (k, v) in map.iter() {
+        println!("k: {} v: {}", k, v);
+    }
 }
 
 fn main() {
-    let a = Thing::A;
+    let arc_map = Arc::new(Mutex::new(Map::new()));
+    handle_n_times(5, arc_map.clone());
 
-    println!("{a:?}");
-    // let n = Thing::B as i32;
+    // todo: join the threads above, wait them till finished
+
+    print_map_items(arc_map.clone());
 }
