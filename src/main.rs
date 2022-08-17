26518ae1038cd33c81de7b5102c9876d00cde49e
YoungHa Kim <ytok1108@aol.com>
2022-08-17 21:09:56 +0900

    Rust hypotenuse__

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,24 @@
-// Thanks: https://stackoverflow.com/a/53894298/665869
-use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
-use std::thread;
-
-type Map = HashMap<String, String>;
-
-fn handle_n_times(count: i32, arc_map: Arc<Mutex<Map>>) {
-    for i in 0..count {
-        let clone_arc = arc_map.clone();
-        thread::scope(|_| {
-            let mut map = clone_arc.lock().unwrap();
-            map.insert(format!("key-{}", i), format!("value-{}", i));
-        });
-    }
+pub trait Hypot<T> {
+    fn hypot(self, b: T) -> f64;
 }
 
-fn print_map_items(arc_map: Arc<Mutex<Map>>) {
-    let clone_arc = arc_map.clone();
-    let map = clone_arc.lock().unwrap();
-    for (k, v) in map.iter() {
-        println!("k: {} v: {}", k, v);
+impl<T> Hypot<T> for T
+where
+    T: core::ops::Mul<T, Output = T>
+        + core::ops::Add<T, Output = T>
+        + core::convert::Into<f64>
+        + Copy,
+{
+    fn hypot(self, b: T) -> f64 {
+        (self * self + b * b).into().sqrt()
     }
 }
 
 fn main() {
-    let arc_map = Arc::new(Mutex::new(Map::new()));
-    handle_n_times(5, arc_map.clone());
+    let x = 3.0_f64;
+    let y = 4.0_f64;
+    let c = x.hypot(y);
 
-    // todo: join the threads above, wait them till finished
-
-    print_map_items(arc_map.clone());
+    println!("hypo : {c}");
 }
+
