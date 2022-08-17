0b51c00e8431768d767681831eee7dd193f0629d
YoungHa Kim <ytok1108@aol.com>
2022-08-17 21:42:54 +0900

    Rust1.63.0 hypotenuse__

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,11 @@
-pub trait Hypot<T> {
-    fn hypot(self, b: T) -> f64;
-}
-
-impl<T> Hypot<T> for T
+pub fn hypot<T>(a: T, b: T) -> f64
 where
     T: core::ops::Mul<T, Output = T>
         + core::ops::Add<T, Output = T>
         + core::convert::Into<f64>
         + Copy,
 {
-    fn hypot(self, b: T) -> f64 {
-        (self * self + b * b).into().sqrt()
-    }
+    (a * a + b * b).into().sqrt()
 }
 
 fn main() {
@@ -19,6 +13,6 @@ fn main() {
     let y = 4.0_f64;
     let c = x.hypot(y);
 
-    println!("hypo : {c}");
+    println!("hypot : {c}");
 }
 
