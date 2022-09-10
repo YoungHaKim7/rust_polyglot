ad6b0567d4bee8536ccc1f5861e215a4c6add847
YoungHa Kim <ytok1108@aol.com>
2022-09-10 19:54:51 +0900

    trait__and lapce

---
+++ src/lapce_rust/target/debug/deps/lapce_rust-be0058608d9f8e23.d
@@ -0,0 +1,5 @@
+/Users/globalyoung/Documents/Project/Github/rust_project/rust_polyglot/src/lapce_rust/target/debug/deps/lapce_rust-be0058608d9f8e23.rmeta: src/main.rs
+
+/Users/globalyoung/Documents/Project/Github/rust_project/rust_polyglot/src/lapce_rust/target/debug/deps/lapce_rust-be0058608d9f8e23.d: src/main.rs
+
+src/main.rs:
---
+++ src/lapce_rust/target/debug/deps/lapce_rust-ffba4a08bfa55a62.d
@@ -0,0 +1,5 @@
+/Users/globalyoung/Documents/Project/Github/rust_project/rust_polyglot/src/lapce_rust/target/debug/deps/lapce_rust-ffba4a08bfa55a62.rmeta: src/main.rs
+
+/Users/globalyoung/Documents/Project/Github/rust_project/rust_polyglot/src/lapce_rust/target/debug/deps/lapce_rust-ffba4a08bfa55a62.d: src/main.rs
+
+src/main.rs:
