433825ecd5721f77e4e52ad70bb9abb8142fb246
YoungHa Kim <ytok1108@aol.com>
2022-09-11 18:48:23 +0900

    generic update code

--- Rust_Lang/generic001/target/debug/deps/generic001-d45011fbe8f2fa48.d
+++
@@ -1,5 +0,0 @@
-/Users/globalyoung/Documents/Project/Github/rust_project/rust_polyglot/Rust_Lang/generic001/target/debug/deps/generic001-d45011fbe8f2fa48.rmeta: src/main.rs
-
-/Users/globalyoung/Documents/Project/Github/rust_project/rust_polyglot/Rust_Lang/generic001/target/debug/deps/generic001-d45011fbe8f2fa48.d: src/main.rs
-
-src/main.rs:
