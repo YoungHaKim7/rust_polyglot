ba9887645a5877e41c55d4d836195b15d4dad371
YoungHa Kim <ytok1108@aol.com>
2022-09-11 16:17:47 +0900

    trait002__let's go

--- Rust_Lang/traits001/target/debug/deps/traits001-0a9ca835c0fb6522.d
+++
@@ -1,7 +0,0 @@
-/Users/globalyoung/Documents/Project/Github/rust_project/rust_polyglot/Rust_Lang/traits001/target/debug/deps/traits001-0a9ca835c0fb6522.rmeta: src/main.rs
-
-/Users/globalyoung/Documents/Project/Github/rust_project/rust_polyglot/Rust_Lang/traits001/target/debug/deps/traits001-0a9ca835c0fb6522.d: src/main.rs
-
-src/main.rs:
-
-# env-dep:CLIPPY_ARGS=
---
+++ Rust_Lang/traits002/src/main.rs
@@ -0,0 +1,65 @@
+struct Dwarf {
+    name: String,
+}
+
+struct Elf {
+    name: String,
+}
+
+struct HalfOrc {
+    name: String,
+}
+
+struct Human {
+    name: String,
+}
+
+impl Constitution for Elf {}
+impl Constitution for Human {}
+
+// The constitution bonus for a dwarf is 2
+impl Constitution for Dwarf {
+    fn constitution_bonus(&self) -> u8 {
+        2
+    }
+}
+
+impl Constitution for HalfOrc {
+    fn constitution_bonus(&self) -> u8 {
+        1
+    }
+}
+
+pub trait Constitution {
+    fn constitution_bonus(&self) -> u8 {
+        0
+    }
+}
+
+fn main() {
+    let my_dwarf = Dwarf {
+        name: String::from("NellDwarf"),
+    };
+
+    let my_elf = Elf {
+        name: String::from("NellElf"),
+    };
+
+    let my_half_orc = HalfOrc {
+        name: String::from("NellHalfOrc"),
+    };
+
+    let my_human = Human {
+        name: String::from("NellHuman"),
+    };
+
+    // Return 2
+    my_dwarf.constitution_bonus();
+
+    // Return 1
+    dbg!(my_human.constitution_bonus());
+
+    // Return 0
+    dbg!(my_elf.constitution_bonus());
+    my_human.constitution_bonus();
+}
