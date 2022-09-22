2ae38fe74e6ab516385eb87680545dd09685a915
YoungHa Kim <ytok1108@aol.com>
2022-09-22 22:00:09 +0900

    ada tutorial

---
+++ Ada_Lang/README.md
@@ -0,0 +1,3 @@
+# Ada Compiler Web
+
+[coding_ground_online_Ada_Compiler](https://www.tutorialspoint.com/compile_ada_online.php)
---
+++ Ada_Lang/helloworld_ada/greet.adb
@@ -0,0 +1,7 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure Greet is
+begin
+    -- Print "Hello, World! Ada Lang" to the screen
+    Ada.Text_IO.Put_Line ("Hello, World ! Ada Lang");
+end Greet;
